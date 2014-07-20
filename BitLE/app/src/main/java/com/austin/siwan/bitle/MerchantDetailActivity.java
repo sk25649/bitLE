7957166ad1929b1f701e8b46313c953234c59661
Si Wan Kim <siwan.kim@utexas.edu>
2014-07-20 09:56:40 -0700

    added map and business detail on merchant detail page

--- BitLE/app/src/main/java/com/austin/siwan/bitle/MerchantDetailActivity.java
+++ BitLE/app/src/main/java/com/austin/siwan/bitle/MerchantDetailActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by Jojo on 7/19/14.
  */
@@ -17,7 +23,19 @@ public class MerchantDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.merchant_detail);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng nestGSV = new LatLng(37.484507, -122.200929);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(nestGSV, 16));
+
+        map.addMarker(new MarkerOptions()
+                .title("Kevin and Joseph's lounge")
+                .snippet("Red Buill and Vodka.")
+                .position(nestGSV));
 
         startTab = (Button)findViewById(R.id.startTab);
         startTab.setOnClickListener(new View.OnClickListener() {
